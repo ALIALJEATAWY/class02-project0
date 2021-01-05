84c80c6a9d395b22ceb6f5987a48baa88cecec95
ali mohammad <des.alialjeatawy@gmail.com>
2021-01-05 21:57:44 +0200

    g

--- js/app.js
+++ js/app.js
@@ -1,213 +1,282 @@
-'use strict';
+// 'use strict';
 
-var username = prompt('What is your name?');
-console.log('userName value', username);
+// var username = prompt('What is your name?');
+// console.log('userName value', username);
 
-var age = prompt('What is your age?');
-console.log('age', age);
+// var age = prompt('What is your age?');
+// console.log('age', age);
 
-var intersets = prompt('What is your interests');
-console.log('intersets', intersets);
+// var intersets = prompt('What is your interests');
+// console.log('intersets', intersets);
 
-alert(' hi ' + username + ' your age ' + age + ' you interest in ' + intersets + ' welcome to my web site ')
+// alert(' hi ' + username + ' your age ' + age + ' you interest in ' + intersets + ' welcome to my web site ')
 
- 1////////////////////////////////////
-var license = prompt('do you have a driving license?');
+//  1////////////////////////////////////
+// var license = prompt('do you have a driving license?');
 
-var testlicense = 'yes';
+// var testlicense = 'yes';
 
-var yes = 'y'
-var y = 'yes'
-var no = 'n'
-var n = 'no'
-
-
+// var yes = 'y'
+// var y = 'yes'
+// var no = 'n'
+// var n = 'no'
 
 
-while (license === null || license === undefined || license === '') {
-  alert('please answer');
-    license = prompt('do you have a driving license?');
-// console.log(license)
-}
 
-while (license.toLowerCase() !== yes && license.toLowerCase() !== no && license.toLowerCase() !== y && license.toLowerCase() !== n)
-{
-    alert('please answer');
-      license = prompt('do you have a driving license?');
-  // console.log(license)
+
+// while (license === null || license === undefined || license === '') {
+//   alert('please answer');
+//     license = prompt('do you have a driving license?');
+// // console.log(license)
+// }
+
+// while (license.toLowerCase() !== yes && license.toLowerCase() !== no && license.toLowerCase() !== y && license.toLowerCase() !== n)
+// {
+//     alert('please answer');
+//       license = prompt('do you have a driving license?');
+//   // console.log(license)
+//   }
+
+
+// if (license.toLowerCase() === testlicense || license.toLowerCase() === "y" )  {
+//     // console.log(license === testlicense || license.toLowerCase() === testlicense);
+//     alert('very, good, you must have a driving license');
+// }
+
+// else if (license.toLowerCase() === "no" || license.toLowerCase() === "n"){
+//     alert('sorry, you cant rent a car without a driving license');
+// // console.log(license)
+// }
+
+
+
+
+// 2//////////////////////////////
+
+// var typelicense = prompt('do you have an automatic driving license?');
+
+// var autolicense = 'yes';
+
+// var yes = 'y'
+// var y = 'yes'
+// var no = 'n'
+// var n = 'no'
+
+
+
+
+// while (typelicense === null || typelicense === undefined || typelicense === '') {
+//   alert('please answer');
+//     typelicense = prompt('do you have a driving license?');
+// // console.log(typelicense)
+// }
+
+// while (typelicense.toLowerCase() !== yes && typelicense.toLowerCase() !== no && typelicense.toLowerCase() !== y && typelicense.toLowerCase() !== n)
+// {
+//     alert('please answer');
+//       typelicense = prompt('do you have a driving license?');
+//   // console.log(typelicense)
+//   }
+
+
+// if (typelicense.toLowerCase() === autolicense || typelicense.toLowerCase() === "y" )  {
+//     // console.log(typelicense === autolicense || typelicense.toLowerCase() === autolicense);
+//     alert('Amazing, you can choose any car');
+// }
+
+// else if (typelicense.toLowerCase() === "no" || typelicense.toLowerCase() === "n"){
+//     alert('we are sorry we only have automatic cars');
+// // console.log(typelicense)
+// }
+
+// 3/////////////////////////////////////////////////////////
+
+// var modern = prompt('do you prefer modern cars over old cars?');
+
+// var moderncar = 'yes';
+
+// var yes = 'y'
+// var y = 'yes'
+// var no = 'n'
+// var n = 'no'
+
+
+
+
+// while (modern === null || modern === undefined || modern === '') {
+//   alert('please answer');
+//     modern = prompt('do you prefer modern cars over old cars?');
+// // console.log(modern)
+// }
+
+// while (modern.toLowerCase() !== yes && modern.toLowerCase() !== no && modern.toLowerCase() !== y && modern.toLowerCase() !== n)
+// {
+//     alert('please answer');
+//       modern = prompt('do you prefer modern cars over old cars?');
+//   // console.log(modern)
+//   }
+
+
+// if (modern.toLowerCase() === moderncar || modern.toLowerCase() === "y" )  {
+//     // console.log(modern === moderncar || modern.toLowerCase() === moderncar);
+//     alert('Thats great, we have a modern cars');
+// }
+
+// else if (modern.toLowerCase() === "no" || modern.toLowerCase() === "n"){
+//     alert('we apologize, all old cars are rented at the moment');
+// // console.log(modern)
+// }
+
+
+// 4////////////////////////////////////////////////////////////////////
+
+
+
+// var con = prompt('do you know the conditions of renting a car?');
+
+// var conrent = 'yes';
+
+// var yes = 'y'
+// var y = 'yes'
+// var no = 'n'
+// var n = 'no'
+
+
+
+
+// while (con === null || con === undefined || con === '') {
+//   alert('please answer');
+//     con = prompt('do you prefer modern cars over old cars?');
+// // console.log(con)
+// }
+
+// while (con.toLowerCase() !== yes && con.toLowerCase() !== no && con.toLowerCase() !== y && con.toLowerCase() !== n)
+// {
+//     alert('please answer');
+//       con = prompt('do you prefer modern cars over old cars?');
+//   // console.log(con)
+//   }
+
+
+// if (con.toLowerCase() === conrent || con.toLowerCase() === "y" )  {
+//     // console.log(con === conrent || con.toLowerCase() === conrent);
+//     alert('Great jop, you know your thing');
+// }
+
+// else if (con.toLowerCase() === "no" || con.toLowerCase() === "n"){
+//     alert('please read the terms and conditions');
+// // console.log(con)
+// }
+
+
+// 5//////////////////////////////////////////////////////
+
+
+
+
+// var service = prompt('do you know the conditions of renting a car?');
+
+// var servicerent = 'yes';
+
+// var yes = 'y'
+// var y = 'yes'
+// var no = 'n'
+// var n = 'no'
+
+
+
+
+// while (service === null || service === undefined || service === '') {
+//   alert('please answer');
+//     service = prompt('do you benefit from our services before?');
+// // console.log(service)
+// }
+
+// while (service.toLowerCase() !== yes && service.toLowerCase() !== no && service.toLowerCase() !== y && service.toLowerCase() !== n)
+// {
+//     alert('please answer');
+//       service = prompt('do you prefer modern cars over old cars?');
+//   // console.log(service)
+//   }
+
+
+// if (service.toLowerCase() === servicerent || service.toLowerCase() === "y" )  {
+//     // console.log(service === servicerent || service.toLowerCase() === servicerent);
+//     alert('Welcom back, check our new services');
+// }
+
+// else if (service.toLowerCase() === "no" || service.toLowerCase() === "n"){
+//     alert('Welcome , to our website');
+// // console.log(service)
+// }
+
+// //////////////////////////////////////////////////////////////
+
+// var game = 12;
+// var gamenumber = prompt('try a number from 2 to 20 you have 4 try')
+
+// var i;
+// for (i = 1; i <= 4; i++) {
+//   console.log(gamenumber);
+//   if (gamenumber == game) {
+//     alert('Right its' + game)
+//     console.log('true');
+//     break;
+//   }
+//   else if (gamenumber > game) {
+//     alert('sorry that is too hight you try ' + i + ' time')
+    
+//     console.log('false');
+//   }
+//   else if (gamenumber < game) {
+//     alert('sorry that is too low you try ' + i + ' time')
+//     console.log('false');
+//   }
+//  gamenumber = prompt('try another time a number from 2 to 20 you have' + i + 'try')
+ 
+// if(gamenumber == game){
+//   alert('correct')
+//   break;
+// }
+//  if(i==3){
+//   alert('sorry, The answer is ' + game);
+//   break; 
+// }
+// }
+
+////////////////////////////////////////////////////////
+
+var car=['TOYOTA'+'MAZDA'+'Honda'+'Porsche'+'TESLA'+'NISSAN'+'KIA'];
+var guess;
+for(var guess=1;guess<=7;guess++){
+  var mycar=prompt('Enter my favorite car')
+  switch(mycar){
+    case 'TOYOTA':
+      case 'MAZDA':
+        case 'Honda':
+          case 'Porsche':
+            case 'TESLA':
+              case 'NISSAN':
+                case 'KIA':
+                  alert('correct, that is one of my favorite cars');
+                  console.log(mycar);
+                  break;
+                  default:
+                  alert('try another one');
+                  console.log(mycar);
+                  
+                  
+                  
+                  
   }
 
+  // if(mycar == car){
+  //   alert('correct')
+  //     break;
+  //   }
+  //   if(mycar==7){
+  //     alert('sorry, The answer is ');
+  //     }
 
-if (license.toLowerCase() === testlicense || license.toLowerCase() === "y" )  {
-    // console.log(license === testlicense || license.toLowerCase() === testlicense);
-    alert('very, good, you must have a driving license');
+
 }
-
-else if (license.toLowerCase() === "no" || license.toLowerCase() === "n"){
-    alert('sorry, you cant rent a car without a driving license');
-// console.log(license)
-}
-
-
-
-
-2//////////////////////////////
-
-var typelicense = prompt('do you have an automatic driving license?');
-
-var autolicense = 'yes';
-
-var yes = 'y'
-var y = 'yes'
-var no = 'n'
-var n = 'no'
-
-
-
-
-while (typelicense === null || typelicense === undefined || typelicense === '') {
-  alert('please answer');
-    typelicense = prompt('do you have a driving license?');
-// console.log(typelicense)
-}
-
-while (typelicense.toLowerCase() !== yes && typelicense.toLowerCase() !== no && typelicense.toLowerCase() !== y && typelicense.toLowerCase() !== n)
-{
-    alert('please answer');
-      typelicense = prompt('do you have a driving license?');
-  // console.log(typelicense)
-  }
-
-
-if (typelicense.toLowerCase() === autolicense || typelicense.toLowerCase() === "y" )  {
-    // console.log(typelicense === autolicense || typelicense.toLowerCase() === autolicense);
-    alert('Amazing, you can choose any car');
-}
-
-else if (typelicense.toLowerCase() === "no" || typelicense.toLowerCase() === "n"){
-    alert('we are sorry we only have automatic cars');
-// console.log(typelicense)
-}
-
-3/////////////////////////////////////////////////////////
-
-var modern = prompt('do you prefer modern cars over old cars?');
-
-var moderncar = 'yes';
-
-var yes = 'y'
-var y = 'yes'
-var no = 'n'
-var n = 'no'
-
-
-
-
-while (modern === null || modern === undefined || modern === '') {
-  alert('please answer');
-    modern = prompt('do you prefer modern cars over old cars?');
-// console.log(modern)
-}
-
-while (modern.toLowerCase() !== yes && modern.toLowerCase() !== no && modern.toLowerCase() !== y && modern.toLowerCase() !== n)
-{
-    alert('please answer');
-      modern = prompt('do you prefer modern cars over old cars?');
-  // console.log(modern)
-  }
-
-
-if (modern.toLowerCase() === moderncar || modern.toLowerCase() === "y" )  {
-    // console.log(modern === moderncar || modern.toLowerCase() === moderncar);
-    alert('Thats great, we have a modern cars');
-}
-
-else if (modern.toLowerCase() === "no" || modern.toLowerCase() === "n"){
-    alert('we apologize, all old cars are rented at the moment');
-// console.log(modern)
-}
-
-
-4////////////////////////////////////////////////////////////////////
-
-
-
-var con = prompt('do you know the conditions of renting a car?');
-
-var conrent = 'yes';
-
-var yes = 'y'
-var y = 'yes'
-var no = 'n'
-var n = 'no'
-
-
-
-
-while (con === null || con === undefined || con === '') {
-  alert('please answer');
-    con = prompt('do you prefer modern cars over old cars?');
-// console.log(con)
-}
-
-while (con.toLowerCase() !== yes && con.toLowerCase() !== no && con.toLowerCase() !== y && con.toLowerCase() !== n)
-{
-    alert('please answer');
-      con = prompt('do you prefer modern cars over old cars?');
-  // console.log(con)
-  }
-
-
-if (con.toLowerCase() === conrent || con.toLowerCase() === "y" )  {
-    // console.log(con === conrent || con.toLowerCase() === conrent);
-    alert('Great jop, you know your thing');
-}
-
-else if (con.toLowerCase() === "no" || con.toLowerCase() === "n"){
-    alert('please read the terms and conditions');
-// console.log(con)
-}
-
-
-5//////////////////////////////////////////////////////
-
-
-
-
-var service = prompt('do you know the conditions of renting a car?');
-
-var servicerent = 'yes';
-
-var yes = 'y'
-var y = 'yes'
-var no = 'n'
-var n = 'no'
-
-
-
-
-while (service === null || service === undefined || service === '') {
-  alert('please answer');
-    service = prompt('do you benefit from our services before?');
-// console.log(service)
-}
-
-while (service.toLowerCase() !== yes && service.toLowerCase() !== no && service.toLowerCase() !== y && service.toLowerCase() !== n)
-{
-    alert('please answer');
-      service = prompt('do you prefer modern cars over old cars?');
-  // console.log(service)
-  }
-
-
-if (service.toLowerCase() === servicerent || service.toLowerCase() === "y" )  {
-    // console.log(service === servicerent || service.toLowerCase() === servicerent);
-    alert('Welcom back, check our new services');
-}
-
-else if (service.toLowerCase() === "no" || service.toLowerCase() === "n"){
-    alert('Welcome , to our website');
-// console.log(service)
-}
-
-
